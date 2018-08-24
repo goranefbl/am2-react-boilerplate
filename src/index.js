069cd5088e02281df80529c6f5bed1249cf4a508
Goran Jakovljevic <goranefbl@gmail.com>
2018-08-24 10:27:58 +0200

    react eslint conflict fix.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Index = () => <div>H ello React!</div>;
-ReactDOM.render(<Index />, document.getElementById('index'));
+const Index = (
+    <div>
+        <h1>Hello React!</h1>
+        <h3>evoa aksd</h3>
+        <p>nest</p>
+    </div>
+);
+ReactDOM.render(Index, document.getElementById('index'));
 
 if (module.hot) {
     module.hot.accept();
